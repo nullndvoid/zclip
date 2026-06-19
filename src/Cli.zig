629584462dd5874cbe501066a860805f5325bd82
J. Hinchliffe <jacob@nullndvoid.xyz>
2026-06-19 21:21:04 +0100

    Cli: add licensing info

--- src/Cli.zig
+++ src/Cli.zig
@@ -1,3 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2026 J. Hinchliffe (nullndvoid)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
 const std = @import("std");
 const Io = std.Io;
 const Allocator = std.mem.Allocator;
