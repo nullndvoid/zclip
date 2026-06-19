03aa40512ba31098c4ad7f874feda914fa96f219
J. Hinchliffe <jjh413@student.bham.ac.uk>
2026-06-19 18:12:35 +0100

    zclip: Make a simple CLI

--- src/main.zig
+++ src/main.zig
@@ -11,9 +11,14 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 
+//! Will be both the client and daemon implementations.
+
 const std = @import("std");
 const Io = std.Io;
+const Allocator = std.mem.Allocator;
 
+const build_options = @import("options");
+const clap = @import("clap");
 const zclip = @import("zclip");
 const Clip = zclip.Clip;
 const Clipboard = zclip.Clipboard;
@@ -22,6 +27,11 @@ const Command = Clipboard.Command;
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
     const alloc = init.gpa;
+
+    setupAndParseArgs(io, alloc, init.minimal.args) catch |err| switch (err) {
+        error.ShouldExit => return,
+        else => return err,
+    };
 
     var arena = std.heap.ArenaAllocator.init(alloc);
     defer arena.deinit();
@@ -52,6 +62,58 @@ pub fn main(init: std.process.Init) !void {
     }
 }
 
+fn setupAndParseArgs(io: Io, alloc: Allocator, args: std.process.Args) !void {
+    var stderr_buf: [1024]u8 = undefined;
+    var stderr_file = Io.File.stderr();
+    defer stderr_file.close(io);
+
+    var file_writer = stderr_file.writer(io, &stderr_buf);
+    var writer = &file_writer.interface;
+
+    var diag: clap.Diagnostic = .{};
+    var res = clap.parse(
+        clap.Help,
+        &params,
+        clap.parsers.default,
+        args,
+        .{
+            .diagnostic = &diag,
+            .allocator = alloc,
+        },
+    ) catch |err| {
+        try diag.reportToFile(io, .stderr(), err);
+        return err;
+    };
+    defer res.deinit();
+
+    if (res.args.help != 0) {
+        try writer.print(preamble, .{});
+        try writer.flush();
+
+        try clap.helpToFile(io, .stderr(), clap.Help, &params, .{});
+        try writer.print(postscript, .{});
+        try writer.flush();
+
+        return error.ShouldExit;
+    } else if (res.args.version != 0) {
+        if (build_options.git_rev) |rev| {
+            try writer.print(
+                "zclip {s} ({s})\n",
+                .{ build_options.version, rev },
+            );
+        } else {
+            try writer.print(
+                "zclip {s}\n",
+                .{build_options.version},
+            );
+        }
+
+        try writer.flush();
+
+        return error.ShouldExit;
+    }
+}
+
 fn parseCommand(input: []const u8) Command {
     const line = std.mem.trim(u8, input, " \t\r");
 
@@ -65,3 +127,25 @@ fn parseCommand(input: []const u8) Command {
         .mime_type = "text/plain;charset=utf-8",
     } };
 }
+
+const preamble =
+    \\ zclip
+    \\
+    \\ A program to manage your clipboard, including over a network.
+    \\
+    \\
+;
+const postscript =
+    \\
+    \\ This program is distributed in the hope that it will be useful,
+    \\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+    \\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    \\ GNU General Public License for more details.
+    \\
+;
+
+const params = clap.parseParamsComptime(
+    \\-h, --help            Display this help and exit
+    \\--version             Show the version of the software
+    \\-v, --verbose         Set the default log level to debug
+);
