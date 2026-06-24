f4f4be8d1674a9970fc755cee470920e74cdce5f
J. Hinchliffe <jacob@nullndvoid.xyz>
2026-06-24 20:43:36 +0100

    zclip/Network: get IO boilerplate going

--- src/Network.zig
+++ src/Network.zig
@@ -19,19 +19,100 @@ const Arena = std.heap.ArenaAllocator;
 const Allocator = std.mem.Allocator;
 const Box = std.crypto.nacl.Box;
 
+const log = std.log.scoped(.net);
+
 io: Io,
-arena: Arena,
+arena: *Arena,
+server: Io.net.Server,
+tasks: Io.Group,
+start_task: Io.Future(anyerror!void),
 
 const Network = @This();
 
-pub fn init(io: Io, arena: Arena) !*Network {
-    const self = try arena.allocator().create(Network);
-    self.* = .{
+const DEFAULT_NET_PORT = 48500;
+
+pub const Config = struct {
+    bind_addr: Io.net.IpAddress = .{ .ip4 = .unspecified(DEFAULT_NET_PORT) },
+};
+
+pub fn init(io: Io, arena: *Arena, config: Config) !Network {
+    var allocating = Io.Writer.Allocating.init(arena.allocator());
+    const writer = &allocating.writer;
+    try config.bind_addr.format(&writer);
+
+    const ip = try allocating.toOwnedSlice();
+    errdefer arena.allocator().free(ip);
+
+    log.debug("Starting listener on {s}", .{ip});
+
+    const server = try config.bind_addr.listen(io, .{
+        .reuse_address = true,
+    });
+
+    return .{
         .io = io,
         .arena = arena,
+        .server = server,
+        .tasks = .init,
+        .start_task = undefined,
     };
+}
 
-    return self;
+/// Starts the Network workers. Blocking. May be cancelled as required.
+pub fn start(self: *Network) anyerror!void {
+    self.start_task = try self.io.concurrent(acceptConnections, .{self});
+    log.debug("Started accepting connections", .{});
+    _ = self.start_task.await(self.io);
+    log.debug("No longer accepting connections", .{});
+}
+
+pub fn deinit(self: Network) void {
+    self.tasks.cancel(self.io);
+    self.start_task.await(self.io);
+    self.server.deinit(self.io);
+}
+
+fn handleConnectionRw(self: *Network, rdr: *Io.Reader, writer: *Io.Writer) anyerror!void {
+    _ = self; // autofix
+    _ = rdr; // autofix
+    log.err("TODO!", .{});
+    try writer.write("NOT YET IMPLEMENTED");
+}
+
+fn handleConnection(self: *Network, stream: Io.net.Stream) anyerror!void {
+    defer stream.shutdown(self.io, .both);
+
+    var read_buf: [4096]u8 = undefined;
+    var write_buf: [4096]u8 = undefined;
+
+    var sock_rdr = stream.reader(self.io, &read_buf);
+    var sock_writer = stream.writer(self.io, &write_buf);
+
+    const rdr = &sock_rdr.interface;
+    const writer = &sock_writer.interface;
+
+    const future = try self.io.concurrent(handleConnectionRw, .{ self, rdr, writer });
+    _ = future.await(self.io);
+}
+
+fn acceptConnections(self: *Network) anyerror!void {
+    defer self.tasks.cancel(self.io);
+
+    while (true) {
+        const stream = self.server.accept(self.io) catch |err| switch (err) {
+            error.Canceled => {
+                return;
+            },
+            else => {
+                log.err("acceptConnections got error: {t}. Moving on...", .{err});
+                continue;
+            },
+        };
+
+        log.debug("Accepted inet connection from peer", .{});
+
+        self.tasks.concurrent(self.io, handleConnection, .{ self, stream });
+    }
 }
 
 pub const Peer = struct {
