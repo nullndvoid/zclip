d13b527c13ba1677602f01f7306b5c1b8be50cab
J. Hinchliffe <jjh413@student.bham.ac.uk>
2026-07-10 20:16:44 +0100

    Config: store dir for updates

--- src/Config.zig
+++ src/Config.zig
@@ -30,10 +30,15 @@ const log = std.log.scoped(.config);
 
 const Config = @This();
 
+/// This field should not be accessed directly. Instead call `Config.get` for a copy.
 data: InnerConfig,
+path: []const u8,
+dir: Io.Dir,
+io: Io,
+alloc: Allocator,
 
 pub fn deinit(self: *Config) void {
-    _ = self; // autofix
+    self.dir.close(self.io);
 }
 
 /// Wrapped in Config because we want to manage the lifetimes of data allocated but
@@ -68,17 +73,28 @@ pub const InnerConfig = struct {
     };
 };
 
-fn parseSlice(allocator: Allocator, data: []const u8, filename: []const u8) !Config {
+fn toSlice(cfg: InnerConfig, allocator: Allocator) ![]const u8 {
+    return try Serde.toml.toSlice(allocator, cfg);
+}
+
+fn parseSlice(io: Io, allocator: Allocator, data: []const u8, path: []const u8, dir: Io.Dir) !Config {
     const cfg = Serde.toml.fromSlice(InnerConfig, allocator, data) catch |err| {
-        log.err("Error parsing config at {s}: {t}", .{ filename, err });
+        log.err("Error parsing config at {s}: {t}", .{ path, err });
         return err;
     };
 
-    return .{ .data = cfg };
+    return .{
+        .data = cfg,
+        .path = path,
+        .io = io,
+        .alloc = allocator,
+        .dir = dir,
+    };
 }
 
-pub fn fromPath(io: Io, allocator: Allocator, path: []const u8) !Config {
-    var config = Io.Dir.cwd().openFile(io, path, .{}) catch |err| {
+/// The opened dir must outlive Config. Just call deinit on this Config.
+fn fromPathWithDir(io: Io, dir: Io.Dir, allocator: Allocator, path: []const u8) !Config {
+    var config = dir.openFile(io, path, .{}) catch |err| {
         switch (err) {
             error.FileNotFound => {
                 log.err("Config file not found at {s}.", .{path});
@@ -99,7 +115,11 @@ pub fn fromPath(io: Io, allocator: Allocator, path: []const u8) !Config {
     const bytes = try rdr.allocRemaining(allocator, .unlimited);
     defer allocator.free(bytes);
 
-    return try parseSlice(allocator, bytes, std.fs.path.basename(path));
+    return try parseSlice(io, allocator, bytes, try allocator.dupe(u8, path), dir);
+}
+
+pub fn fromPath(io: Io, allocator: Allocator, path: []const u8) !Config {
+    return try fromPathWithDir(io, Io.Dir.cwd(), allocator, path);
 }
 
 /// TODO: Use a Well known location e.g. $XDG_CONFIG_DIR/zclip/zclip.zon.
@@ -124,4 +144,113 @@ pub fn fromWellKnown(io: Io, allocator: Allocator, environ: *const std.process.E
     defer allocator.free(config_path);
 
     return try fromPath(io, allocator, config_path);
+}
+
+/// Gets a copy of the configuration data.
+pub fn get(cfg: Config) InnerConfig {
+    return cfg.data;
+}
+
+/// Updates the inner config. Call `Config.get` for a copy of the internal data.
+/// Do not update `.data` directly, this is in case an update fails.
+pub fn update(cfg: *Config, new_config: InnerConfig) !void {
+    const bytes = try toSlice(new_config, cfg.alloc);
+
+    var atomic_file = try cfg.dir.createFileAtomic(
+        cfg.io,
+        cfg.path,
+        .{ .replace = true },
+    );
+    defer atomic_file.deinit(cfg.io);
+
+    var writer_buf: [1024]u8 = undefined;
+
+    var af_writer = atomic_file.file.writer(cfg.io, &writer_buf);
+    const writer = &af_writer.interface;
+
+    try writer.writeAll(bytes);
+    try writer.flush();
+
+    try atomic_file.replace(cfg.io);
+
+    cfg.data = new_config;
+}
+
+test "read config" {
+    const io = std.testing.io;
+    const alloc = std.testing.allocator;
+    var arena = std.heap.ArenaAllocator.init(alloc);
+    defer arena.deinit();
+
+    const tmp_dir = std.testing.tmpDir(.{});
+
+    // Write the config to test/config.toml.
+    {
+        const config =
+            \\[daemon]
+            \\bind_address = "127.0.0.1:48501"
+            \\unix_socket_address = "./data/zclip_bob.sock"
+            \\data_dir = "./data"
+            \\
+            \\[[daemon.peers]]
+            \\nickname = "Alice"
+            \\pubkey = "4EIP8HSpq2hx2jhqVOPWXf5Ri0JTdzTa5iP6091VTDc="
+            \\addr = "127.0.0.1:48500"
+        ;
+
+        const file = try tmp_dir.dir.createFile(io, "config.toml", .{});
+        defer file.close(io);
+
+        var write_buf: [1024]u8 = undefined;
+        var file_writer = file.writer(io, &write_buf);
+        const writer = &file_writer.interface;
+
+        try writer.writeAll(config);
+        try writer.flush();
+    }
+
+    const cfg = try Config.fromPathWithDir(io, tmp_dir.dir, arena.allocator(), "config.toml");
+
+    const data = cfg.get();
+
+    try std.testing.expectEqualStrings("./data", data.daemon.data_dir.?);
+    try std.testing.expectEqualStrings("Alice", data.daemon.peers.?[0].nickname);
+}
+
+test Config {
+    const io = std.testing.io;
+    const alloc = std.testing.allocator;
+    var arena = std.heap.ArenaAllocator.init(alloc);
+    defer arena.deinit();
+
+    // To avoid clobbering the example config on call to update.
+    const f = try Io.Dir.createFileAbsolute(io, "/tmp/zclip.toml", .{});
+    f.close(io);
+
+    {
+        var cfg = try Config.fromPath(io, arena.allocator(), "/tmp/zclip.toml");
+        var cfg_data = cfg.get();
+
+        const peers = cfg_data.daemon.peers;
+
+        _ = peers; // Do something with peers.
+
+        cfg_data.client.unix_socket_address = "/tmp/zclip.sock";
+
+        try cfg.update(cfg_data); // Updates the config with new data.
+
+    }
+
+    // Now we could re-open cfg and assert same.
+    var tmp_dir = try Io.Dir.openDirAbsolute(io, "/tmp", .{});
+    defer tmp_dir.close(io);
+
+    var cfg = try Config.fromPathWithDir(io, tmp_dir, arena.allocator(), "zclip.toml");
+
+    const cfg_data = cfg.get();
+
+    try std.testing.expectEqualStrings(
+        "/tmp/zclip.sock",
+        cfg_data.client.unix_socket_address.?,
+    );
 }
